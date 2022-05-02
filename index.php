7af74193f43c6c9ac80c816f4ac1682ec714cb82
redex2 <redex2@redex2.dev>
2022-05-02 21:41:58 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+ header ('content-type: image/svg+xml'); 
+ date_default_timezone_set("UTC");
+ $filename="counter";
+ $myfile = fopen($filename, "a+");
+ $data=0;
+ if(filesize($filename)) $data=intval(fread($myfile, filesize($filename)));
+ ftruncate($myfile, 0);
+ rewind($myfile);
+ $data=strval($data+1);
+ fwrite($myfile, $data);
+ fclose($myfile);
+?>
+<svg xmlns="http://www.w3.org/2000/svg" width="<?php echo (strlen(str_replace(" ", "",$data))*8)+89;?>" height="20">
+	<rect width="81" height="20" fill="#555"/>
+	<rect x="81" width="<?php echo (strlen(str_replace(" ", "",$data))*8)+6;?>" height="20" fill="#97ca00"/>
+	<text x="41.5" y="14" fill="#fff" text-anchor="middle" font-size="11" font-family="DejaVu Sans">Profile views</text>
+	<text x="<?php echo (strlen(str_replace(" ", "",$data))*4)+84;?>" y="14" fill="#fff" text-anchor="middle" font-size="11" font-family="DejaVu Sans"><?php echo number_format($data, 0, "", " ");?></text>
+</svg>
